155ce379e2221fac6e418c5e8e97ce79d3e67001
Mitzi Estrada <m_estrada17@live.com>
2020-02-12 17:42:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
+  if phrase = phrase.upcase
     return 'NO, NOT SINCE 1938!'
-  elsif phrase == 'I LOVE YOU GRANDMA'
+  elsif phrase = 'I LOVE YOU GRANDMA'
     return 'I LOVE YOU TOO PUMPKIN'
   else
     return 'HUH?! SPEAK UP, SONNY'
